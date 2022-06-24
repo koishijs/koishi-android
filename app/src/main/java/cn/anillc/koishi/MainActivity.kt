c1765bee0243eb0900b1e9725757ca6c9d0621d1
Anillc <i@anillc.cn>
2022-06-24 17:00:01 +0800

    init android project

---
+++ app/src/main/java/cn/anillc/koishi/MainActivity.kt
@@ -0,0 +1,11 @@
+package cn.anillc.koishi
+
+import android.app.Activity
+import android.os.Bundle
+
+class MainActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main)
+    }
+}
