2fddf0353eb5d2165096d4d5853f6bafe4f93740
Il Harper <hi@ilharper.com>
2022-07-07 08:20:02 +0800

    feat: add `StartActivity`
    Add new activities, but not replace the old ones.
    We'll replace them all in one commit at last.

---
+++ app/src/main/java/cn/anillc/koishi/activities/StartActivity.kt
@@ -0,0 +1,138 @@
+package cn.anillc.koishi.activities
+
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.compositionLocalOf
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import cn.anillc.koishi.R
+import cn.anillc.koishi.ui.theming.KoishiTheme
+
+class StartActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            StartCompose()
+        }
+    }
+}
+
+val LocalNavHostController = compositionLocalOf<NavHostController?> { null }
+
+@Composable
+fun StartCompose() {
+    // Navigation control
+    val nav = rememberNavController()
+
+    // Content
+    KoishiTheme {
+        CompositionLocalProvider(
+            LocalNavHostController provides nav
+        ) {
+            NavHost(
+                modifier = Modifier.fillMaxSize(),
+                navController = nav,
+                startDestination = "welcome"
+            ) {
+                composable("welcome") { TabWelcome() }
+                composable("request_permission") { TabRequestPermission() }
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TabScaffold(
+    enabled: Boolean = true,
+    prev: Boolean = true,
+    next: String = "",
+    content: @Composable (PaddingValues) -> Unit
+) {
+    val colorSurface = MaterialTheme.colorScheme.surface
+
+    val nav = LocalNavHostController.current!!
+
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        containerColor = MaterialTheme.colorScheme.primaryContainer,
+        bottomBar = {
+            BottomAppBar(
+                modifier = Modifier
+                    .drawBehind {
+                        drawLine(
+                            colorSurface,
+                            Offset(0f, 0f),
+                            Offset(size.width, 0f),
+                            1 * density // 1dp
+                        )
+                    },
+                containerColor = MaterialTheme.colorScheme.primaryContainer,
+                tonalElevation = Dp(0f)
+            ) {
+                if (prev) {
+                    TextButton(
+                        enabled = enabled,
+                        onClick = { nav.navigateUp() }
+                    ) {
+                        Text(stringResource(R.string.koishi_common_prev))
+                    }
+                }
+                Spacer(Modifier.weight(1f, true))
+                if (next.isNotEmpty()) {
+                    TextButton(
+                        enabled = enabled,
+                        onClick = { nav.navigate(next) }
+                    ) {
+                        Text(stringResource(R.string.koishi_common_next))
+                    }
+                }
+            }
+        },
+        content = content
+    )
+}
+
+@Composable
+fun TabWelcome() {
+    TabScaffold(
+        next = "request_permission",
+        prev = false
+    ) {
+        // TODO
+    }
+}
+
+@Composable
+fun TabRequestPermission() {
+    TabScaffold {
+        // TODO
+    }
+}
+
+@Preview
+@Composable
+fun StartComposePreview() {
+    StartCompose()
+}
