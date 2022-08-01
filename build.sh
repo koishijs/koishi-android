08f9da9ce35c991558bec4e5319b4e065e9dec18
Anillc <i@anillc.cn>
2022-08-01 13:09:09 +0800

    fix build script

--- build.sh
+++ build.sh
@@ -16,12 +16,8 @@ buildAssets() {
     popd
 
     pushd app/src/main/assets/bootstrap
-    RELEASE=$(curl \
-        -H "Accept: application/vnd.github.v3+json" \
-        'https://api.github.com/repos/koishijs/boilerplate/releases/latest')
-    BP=$(echo "$RELEASE" | jq -r '.assets[0].browser_download_url')
-    checkStatus $? 'boilerplate 查询失败。'
-    curl -o koishi.zip $BP
+    BP='https://github.com/koishijs/boilerplate/releases/download/v1.0.3/boilerplate-v1.0.3-linux-arm64-node16.zip'
+    curl -L -o koishi.zip $BP
     checkStatus $? 'boilerplate 下载失败。'
     curl -O https://repo.yarnpkg.com/3.2.0/packages/yarnpkg-cli/bin/yarn.js
     checkStatus $? 'yarn.js 下载失败。'
