68f9efe59baea66a51e55b84dba41c7a0883cf02
lipraty <sis1943@outlook.com>
2023-07-20 06:09:55 +0800

    add naive ui

--- packages/core/src/App.vue
+++ packages/core/src/App.vue
@@ -1,7 +1,7 @@
-<script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-</script>
-
 <template>
   <RouterView />
 </template>
+
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
