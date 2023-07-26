b8f36fa3149e0a298c4782320ed86b785afd5509
lipraty <sis1943@outlook.com>
2023-07-26 16:20:07 +0800

    support translucent status bar

--- packages/core/android/app/src/main/java/cn/anillc/koishi/MainActivity.java
+++ packages/core/android/app/src/main/java/cn/anillc/koishi/MainActivity.java
@@ -1,5 +1,17 @@
 package cn.anillc.koishi;
 
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+    }
+}
