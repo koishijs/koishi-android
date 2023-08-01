31ca6bda714d6482abb5418f2a396445ebff9874
lipraty <sis1943@outlook.com>
2023-08-01 10:34:27 +0800

    add TitleLayout

--- packages/core/src/views/Instance.vue
+++ packages/core/src/views/Instance.vue
@@ -1,7 +1,12 @@
 <template>
+  <title-layout>
+    <template #text>Koishi</template>
+    123
+  </title-layout>
 </template>
 
 <script lang="ts" setup>
+import TitleLayout from '@/components/TitleLayout.vue'
 </script>
 
 <style scoped>
