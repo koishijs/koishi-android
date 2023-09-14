716cf2b52d8de797de6a54a39751e43692c37523
lipraty <sis1943@outlook.com>
2023-09-15 01:12:58 +0800

    remove deprecated code

--- packages/core/android/app/src/main/java/cn/anillc/koishi/MainActivity.kt
+++ packages/core/android/app/src/main/java/cn/anillc/koishi/MainActivity.kt
@@ -1,14 +1,5 @@
 package cn.anillc.koishi
 
-import android.os.Bundle
-import android.view.View
-import android.view.WindowManager
 import com.getcapacitor.BridgeActivity
 
-class MainActivity : BridgeActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
-        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
-    }
-}
+class MainActivity : BridgeActivity()
