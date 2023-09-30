79acae254d2a6aaa753e78dfbeba6f63ec399697
lipraty <sis1943@outlook.com>
2023-09-30 09:47:42 +0800

    update WebUI layout

--- packages/core/src/views/WebUI.vue
+++ packages/core/src/views/WebUI.vue
@@ -1,8 +1,8 @@
 <template>
   <title-layout>
     <template #text>WebUI</template>
-    <template #left><img src="../assets/back.svg" @click="$router.go(-1)"/></template>
-    <iframe :src="src" frameborder="0"></iframe>
+    <template #left><img src="../assets/back.svg" @click="$router.push('/instances')"/></template>
+    <iframe :src="src" frameborder="0" class="webui"></iframe>
   </title-layout>
 </template>
 
@@ -13,3 +13,11 @@ import TitleLayout from '../components/TitleLayout.vue'
 const route = useRoute()
 const src = route.query.url as string
 </script>
+
+<style scoped>
+.webui {
+  width: 100%;
+  height: 100%;
+  box-shadow: 0px 0px 24px rgba(85, 70, 163, 0.30);
+}
+</style>
